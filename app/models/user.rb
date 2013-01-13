b41645c17ba0f8100e4cf9c183378c82d803f0ac
Iftode Bogdan <iftode.bogdan@gmail.com>
2013-01-13 07:45:13 +0200

    Added username field in User table

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,10 +3,28 @@ class User < ActiveRecord::Base
   # :token_authenticatable, :confirmable,
   # :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable, :authentication_keys => [:login]
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me
+  # Virtual attribute for authenticating by either username or email
+  # This is in addition to a real persisted field like 'username'
+  attr_accessor :login
+  attr_accessible :username, :email, :password, :password_confirmation, :remember_me
+  attr_accessible :login
   # attr_accessible :title, :body
   has_many :posts, :dependent => :destroy
+
+   
+    def self.find_first_by_auth_conditions(warden_conditions)
+      conditions = warden_conditions.dup
+      if login = conditions.delete(:login)
+        where(conditions).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+      else
+        where(conditions).first
+      end
+    end
+
+### This is the correct method you override with the code above
+### def self.find_for_database_authentication(warden_conditions)
+### end 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130112161500) do
+ActiveRecord::Schema.define(:version => 20130113051731) do
 
   create_table "albums", :force => true do |t|
     t.string   "album_name"
@@ -56,6 +56,7 @@ ActiveRecord::Schema.define(:version => 20130112161500) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                             :null => false
     t.datetime "updated_at",                             :null => false
+    t.string   "username"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
